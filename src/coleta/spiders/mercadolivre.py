11f9d53838ff97d3105219a6623638876bb9f894
Fabio Lucas <fabiolucamz@gmail.com>
2025-12-11 18:42:29 -0300

    first commit

---
+++ src/coleta/spiders/mercadolivre.py
@@ -0,0 +1,29 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+
+    def parse(self, response):
+        products = response.css('div.ui-search-result__wrapper') #57 itens
+        
+
+        for product in products:
+            prices= product.css('span.andes-money-amount__fraction::text').getall()
+            cents= product.css('span.andes-money-amount__cents::text').getall()
+            rating = product.css('span.poly-component__review-compacted span::text').getall()
+            yield{
+                'title' : product.css('a.poly-component__title::text').get(),
+                'brand' : product.css('span.poly-phrase-label.poly-fw-semibold::text').get(),
+                'reviews_rating_number' : rating[0] if len(rating) > 0 else None,
+                'reviews_amount' : rating[1].replace('| ', '') if len(rating) > 1 else None,
+                'old_price_amount' : prices[0] if len(prices) > 0 else None,
+                'old_price_cents' : cents[0] if len(cents) > 0 else None,
+                'price_amount' : prices[1] if len(prices) > 1 else None,
+                'price_cents' : cents[1] if len(cents) > 1 else None,
+            }
+                
+
+            
